f63dab4b8df94792fb03e47646fec65193c66953
Derek-Roberts <droberts16@mail.csuchico.edu>
2016-06-14 16:26:36 -0700

    The Index loads the first and last words from the 4-letter words dictionary and uses the first and last for step0 and the goal

--- wladder/app/controllers/home_controller.rb
+++ wladder/app/controllers/home_controller.rb
@@ -1,17 +1,22 @@
 class HomeController < ApplicationController
 
+  load "#{Rails.root}/lib/ladder.rb"
+
   def index
-      # don't need to do anything here
+    # Pick a random word from the dictionary
+    @start_word = dictionary.first
+    @end_word = dictionary.last
   end
 
   def results
+    #legal steps
     #@query_text = params[:query]
     #if @query_text =~ /^[a-zA-Z0-9\.\-]*$/
     #  @result = `whois #{@query_text}`.split("\n")
     #else
     #  @result = ["Illegal URL"];
     #end
-    @input = [params[:step1], params[:step2], params[:step3], params[:step4], params[:step5]] 
+    @steps = [params[:step1], params[:step2], params[:step3], params[:step4], params[:step5]] 
   end
 
 end
