c958d0c9003ac54701812ce515af4b66e82073de
Derek-Roberts <droberts16@mail.csuchico.edu>
2016-06-14 14:43:16 -0700

    Changed the index view and created a results page that displays the user provided inputs for each step

--- wladder/app/controllers/home_controller.rb
+++ wladder/app/controllers/home_controller.rb
@@ -4,13 +4,14 @@ class HomeController < ApplicationController
       # don't need to do anything here
   end
 
-  def show
-    @query_text = params[:query]
-    if @query_text =~ /^[a-zA-Z0-9\.\-]*$/
-      @result = `whois #{@query_text}`.split("\n")
-    else
-      @result = ["Illegal URL"];
-    end
+  def results
+    #@query_text = params[:query]
+    #if @query_text =~ /^[a-zA-Z0-9\.\-]*$/
+    #  @result = `whois #{@query_text}`.split("\n")
+    #else
+    #  @result = ["Illegal URL"];
+    #end
+    @input = [params[:step1], params[:step2], params[:step3], params[:step4], params[:step5]] 
   end
 
 end
--- wladder/config/routes.rb
+++ wladder/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root "home#index"
-  get 'check', to: 'home#show'
-  get 'step1', to: 'home#show'
-  get 'step2', to: 'home#show'
-  get 'step3', to: 'home#show'
-  get 'step4', to: 'home#show'
-  get 'step5', to: 'home#show'
+  get 'check', to: 'home#results'
+  get 'step1', to: 'home#results'
+  get 'step2', to: 'home#results'
+  get 'step3', to: 'home#results'
+  get 'step4', to: 'home#results'
+  get 'step5', to: 'home#results'
 end
