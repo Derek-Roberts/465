3be945c2c9ec7380c503c8dff0559b62a6b27a56
Derek-Roberts <droberts16@mail.csuchico.edu>
2016-06-21 00:11:53 -0700

    Added the show page along with the ability to create new references

--- wbib/app/controllers/references_controller.rb
+++ wbib/app/controllers/references_controller.rb
@@ -5,4 +5,24 @@ class ReferencesController < ApplicationController
     @references = Reference.all
   end
 
+  # GET /references/new
+  def new
+    @reference = Reference.new
+  end
+  
+  # POST /references
+  def create
+    @reference = Reference.new(params.require(:reference).permit(:url, :topic, :annotation, :creationtime))
+    if @reference.save
+      redirect_to @reference, notice: 'Reference was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # GET /references/1
+  def show
+    @reference = Reference.find(params[:id])
+  end
+
 end
