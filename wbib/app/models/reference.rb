566778fd060d5bd985f63a18b138f5c270c2131c
Derek-Roberts <droberts16@mail.csuchico.edu>
2016-06-21 17:49:37 -0700

    Working production version now after changing the config/environments/production.rb file to allow asset compilation

--- wbib/app/models/reference.rb
+++ wbib/app/models/reference.rb
@@ -1,7 +1,8 @@
 class Reference < ActiveRecord::Base
   before_validation :add_http
 
-  validates :url, presence: true, url: true
+  # Checks the validity of the URL through a regex by Diego found at this link: https://gist.github.com/dperini/729294
+  validates :url, presence: true, format: { with: /\A(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?\z/i, message: "invalid." }
   validates :topic, presence: true
   validates :annotation, presence: true
 
