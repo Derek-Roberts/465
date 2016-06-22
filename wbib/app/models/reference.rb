d12e8af5dab61d98023e5592471bfd18b3c88b37
Derek-Roberts <droberts16@mail.csuchico.edu>
2016-06-21 19:52:42 -0700

    Forgot to check for blank strings on Topic and Annotation input.

--- wbib/app/models/reference.rb
+++ wbib/app/models/reference.rb
@@ -3,8 +3,9 @@ class Reference < ActiveRecord::Base
 
   # Checks the validity of the URL through a regex by Diego found at this link: https://gist.github.com/dperini/729294
   validates :url, presence: true, format: { with: /\A(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?\z/i, message: "invalid." }
-  validates :topic, presence: true
-  validates :annotation, presence: true
+  #validates :topic, presence: true, format: { with: /^[\S]+$/, message: "invalid.  Just whitespace was present."}
+  validates :topic, presence: true, allow_blank: false
+  validates :annotation, presence: true, allow_blank: false
 
   def add_http
     unless self.url =~ /^https?:\/\//
