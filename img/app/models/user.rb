40a976bf21a50bfef04ad24168c97b3f53943486
Derek-Roberts <droberts16@mail.csuchico.edu>
2016-06-23 00:41:47 -0700

    Added the through => :ImageUsers to the User model and tweaked the Image index page a bit

--- img/app/models/user.rb
+++ img/app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :Images
+  has_many :Images, through: :ImageUsers
   has_many :ImageUsers
+
 end
