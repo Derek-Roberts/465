659fced237ba2f52c3daf0177c477b4583622e2c
Derek-Roberts <droberts16@mail.csuchico.edu>
2016-06-22 23:54:23 -0700

    Had to change every instance of private_flag in the Image object to just private.  Thank god for that first ruby program we wrote right?

--- img/app/controllers/images_controller.rb
+++ img/app/controllers/images_controller.rb
@@ -74,6 +74,6 @@ class ImagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
-      params.require(:image).permit(:filename, :private_flag, :user_id)
+      params.require(:image).permit(:filename, :private, :user_id)
     end
 end
