2a5542c4000f0f6da10cdd01146378b06f648f29
Derek-Roberts <droberts16@mail.csuchico.edu>
2016-06-22 23:44:26 -0700

    Added the name field to the User object created by devise and fixed the private column of the Image object so it would work with the provided seedfile.

--- img/app/models/image.rb
+++ img/app/models/image.rb
@@ -1,5 +1,7 @@
 class Image < ActiveRecord::Base
+  belongs_to :Users
   has_many :ImageUsers
   # Delete the image's tags when the image has been deleted
   has_many :Tags, dependent: :destroy
+  has_many :Users, through: :ImageUsers
 end
--- img/db/schema.rb
+++ img/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623014850) do
+ActiveRecord::Schema.define(version: 20160623062011) do
 
   create_table "image_users", force: :cascade do |t|
     t.integer  "image_id"
@@ -22,10 +22,10 @@ ActiveRecord::Schema.define(version: 20160623014850) do
 
   create_table "images", force: :cascade do |t|
     t.string   "filename"
-    t.boolean  "private_flag"
+    t.boolean  "private"
     t.integer  "user_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tags", force: :cascade do |t|
@@ -34,5 +34,25 @@ ActiveRecord::Schema.define(version: 20160623014850) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "name"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["name"], name: "index_users_on_name", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
