af8942060e30964138fae9129a6dd6b2ce96003b
Derek-Roberts <droberts16@mail.csuchico.edu>
2016-06-24 01:29:48 -0700

    Changed a few things in the images model so the database could be seeded

--- img/app/models/image.rb
+++ img/app/models/image.rb
@@ -1,16 +1,16 @@
 class Image < ActiveRecord::Base
-  before_validation :available_users
+  #before_validation :available_users
+  has_many :Users, through: :ImageUsers
+  accepts_nested_attributes_for :Users
 
   belongs_to :Users
   has_many :ImageUsers
 
   # Delete the image's tags when the image has been deleted
   has_many :Tags, dependent: :destroy
-  has_many :Users, through: :ImageUsers
 
-  def available_users
-    users_array = User.all - self.users - [self.user]
-    # Need to format here with username then email
-    
-  end
+  #def available_users
+    #users_array = User.all - self.user - [self.user]
+    # Need to format here with username then email 
+  #end
 end
