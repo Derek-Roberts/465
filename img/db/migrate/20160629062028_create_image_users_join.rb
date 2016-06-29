c7ac90773ea6d6ac03ab6fb8b235cec6e7be3580
Derek-Roberts <droberts16@mail.csuchico.edu>
2016-06-28 23:32:58 -0700

    Messed up my migration.  FUCK

--- img/db/migrate/20160629062028_create_image_users_join.rb
+++
@@ -1,9 +0,0 @@
-class CreateImageUsersJoin < ActiveRecord::Migration
-  def change
-    create_table :image_users, :id => false do |t|
-      t.integer "image_id"
-      t.integer "user_id"
-    end
-    add_index :image_users, ["image_id", "user_id"]
-  end
-end
