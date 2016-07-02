d7484e287b296e2961d0f286441fbf1be5f95d55
Derek-Roberts <droberts16@mail.csuchico.edu>
2016-07-01 21:26:20 -0700

    Now displays the currently shared users, still working on the actual sharing part though

--- img/app/models/image.rb
+++ img/app/models/image.rb
@@ -15,11 +15,27 @@ class Image < ActiveRecord::Base
   #   return true
   # end
   # return false
+  def current_shared
+    shared_image_users = ImageUser.all.map { |user| user if user.image_id == self.id }.compact
+    shared_users = shared_image_users.map { |u| User.find(u.user_id) }.compact
+    user = User.all.map {|u| u if u.id == self.user_id }.compact
+    users_array = shared_users - user 
+    users_array.map { |user| [user.name + " (" + user.email + ")", user.id]}
+  end
 
   def available_users
     #users_array = User.all - self.user_id - [self.user_id]
+    #users_array = User.all - self.users
+    #current_user = current_user
+    shared_image_users = ImageUser.all.map { |user| user if user.image_id == self.id }.compact
+    shared_users = shared_image_users.map { |u| User.find(u.user_id) }.compact
+    user = User.all.map {|u| u if u.id == self.user_id }.compact
+    users_array = User.all - shared_users - user 
+    #users = User.all.map { |user| user if user.id.includes? shared_users }.compact
+    #users_array = ImageUser.all.map { |u| ImageUser.find(self.id) }.compact
+    #@usernames = @images.map { |image| User.find(image.user_id).name }
     # Need to format here with username then email 
-    #users_array.map { |user| [user.name + " (" + user.email + ")", user.id]}
+    users_array.map { |user| [user.name + " (" + user.email + ")", user.id]}
   end
   # 
   # Remove image file
